1ed0640216286f5e5f51c6945c205a0ee0f62f6e
Christian Rua <christian.rua@autodesk.com>
2022-09-10 15:06:40 -0500

    fixing the GladLib class

--- src/com/dukeCourse3/week2/TesterClass.java
+++ src/com/dukeCourse3/week2/TesterClass.java
@@ -6,8 +6,8 @@ import com.dukeCourse3.week2.WordFrequencies;
 public class TesterClass {
 
     public static void main(String[] args) {
-        CharactersInPlay tc = new CharactersInPlay();
-        tc.characterWithNumParts(10,15);
+        GladLib tc = new GladLib();
+        tc.makeStory();
     }
 
 }
