0741bb67228c287b6478eeed1fbee00aa862cb7e
christian.rua@gmail.com <christian.rua@gmail.com>
2022-11-26 16:18:42 -0500

    adding week4 content

--- src/com/dukeCourse3/week4/VigenereBreaker.java
+++ src/com/dukeCourse3/week4/VigenereBreaker.java
@@ -1,4 +1,5 @@
 package com.dukeCourse3.week4;
+import java.lang.reflect.Array;
 import java.util.*;
 import edu.duke.*;
 
@@ -7,6 +8,15 @@ public class VigenereBreaker {
         StringBuilder sb = new StringBuilder();
         for (int i = whichSlice; i < message.length(); i += totalSlices){
                 sb.append(message.charAt(i));
+        }
+        return sb.toString();
+    }
+
+    public String getKeyRepresentation(int[] keyLength) {
+        StringBuilder sb = new StringBuilder();
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toLowerCase();
+        for (int i = 0; i < keyLength.length; i++){
+            sb.append(alphabet.charAt(keyLength[i]));
         }
         return sb.toString();
     }
@@ -28,6 +38,63 @@ public class VigenereBreaker {
         String vigenereMessage = fr.toString();
         VigenereBreaker vb = new VigenereBreaker();
 
+    }
+
+    public HashSet<String> readDictionary(FileResource fr){
+        HashSet<String> wordsDict = new HashSet<>();
+        for(String word : fr.lines()){
+            wordsDict.add(word.toLowerCase());
+        }
+
+        return wordsDict;
+    }
+    public int countWords(String message, HashSet<String> dictionary){
+        int validWordsValue = 0;
+        for(String word : message.split("\\W")){
+            if(dictionary.contains(word.toLowerCase())){
+                validWordsValue = validWordsValue + 1;
+            }
+        }
+
+        return validWordsValue;
+    }
+
+    public String breakForLanguage(String encrypted, HashSet<String> dictionary){
+        int bestRealWordsCount = 0;
+        String bestDecryptedMessage = "";
+        String keyValue = "";
+        String bestKeyLengthString = "";
+
+        for(int i=1; i <= 100; i++){
+            int[] keyLength = tryKeyLength(encrypted,i,'e');
+            int[] bestKeyLength = new int[keyLength.length];
+            VigenereCipher vc = new VigenereCipher(keyLength);
+            String decryptedMessage = vc.decrypt(encrypted);
+            int numberOfRealWords = countWords(decryptedMessage, dictionary);
+            if(numberOfRealWords > bestRealWordsCount){
+                bestRealWordsCount = numberOfRealWords;
+                bestDecryptedMessage = decryptedMessage;
+                bestKeyLength = keyLength;
+                keyValue = getKeyRepresentation(bestKeyLength);
+                bestKeyLengthString = Arrays.toString(bestKeyLength);
+            }
+        }
+
+        System.out.println("bestRealWordsCount value " + bestRealWordsCount);
+        System.out.println("bestKeyLength value " + bestKeyLengthString);
+        System.out.println("keyValue value " + keyValue);
+        return bestDecryptedMessage;
+
+    }
+
+
+    public void breakVigenere() {
+        FileResource fr = new FileResource();
+        String message = fr.asString();
+        FileResource languageContent = new FileResource("week4Data/dictionaries/English");
+        HashSet<String> languageDict = readDictionary(languageContent);
+        String decryptedMessage = breakForLanguage(message, languageDict);
+        System.out.println("the decrypted message is " + decryptedMessage);
     }
 
 }
--- src/com/dukeCourse3/week4/testClass6.java
+++ src/com/dukeCourse3/week4/testClass6.java
@@ -1,6 +1,7 @@
 package com.dukeCourse3.week1;
 
 import com.dukeCourse3.week4.*;
+import com.dukeCourse3.week4.CaesarCipher;
 import edu.duke.FileResource;
 
 import java.io.File;
@@ -8,11 +9,27 @@ import java.util.Arrays;
 
 public class testClass6 {
 
-    public static void main(String[] args) {
+    public static void caesarCipherTester(){
+        CaesarCipher cc = new CaesarCipher(5);
+        FileResource fr = new FileResource("week4Data/titus-small.txt");
+        String encryptedMessage = cc.encrypt(fr.asString());
+        System.out.println("encryptedMessage value " + encryptedMessage);
+        String decryptedMessage = cc.decrypt(encryptedMessage);
+        System.out.println("decryptedMessage value " + decryptedMessage);
+    }
+
+    public static void tryKeyLengthTest(){
         VigenereBreaker vb = new VigenereBreaker();
         FileResource fr = new FileResource("week4Data/athens_keyflute.txt");
-        int[]  response = vb.tryKeyLength(fr.toString(),5,'e');
+        int[]  response = vb.tryKeyLength(fr.asString(),5,'e');
         System.out.println("response value " + Arrays.toString(response));
+    }
+
+    public static void main(String[] args) {
+        //caesarCipherTester();
+        //tryKeyLengthTest()
+        VigenereBreaker vb = new VigenereBreaker();
+        vb.breakVigenere();
 
     }
 }
